53901124ce413860b063daf346266a421cc95057
Vikash Sharma <vikash@mpaani.com>
2019-10-02 18:14:48 +0530

    Ceated ErrorBoundry Handler

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/theme.scss';
 import { Container, Row, Col } from 'react-bootstrap';
+import ErrorHandler from './components/ErrorHandler';
+import BuggyCounter from './components/BuggyCounter';
 import './App.scss';
 
 function App() {
@@ -11,6 +13,13 @@ function App() {
         <Col>
           <h4>Test React App</h4>
         </Col>
+      </Row>
+      <Row>
+        <ErrorHandler>
+          <p>Something Went wrong</p>
+          <BuggyCounter />
+          <BuggyCounter />
+        </ErrorHandler>
       </Row>
     </Container>
   );
---
+++ src/app/components/BuggyCounter.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+// import ErrorHandler from './ErrorHandler';
+
+class BuggyCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  handleClick = () => {
+    this.setState(({ counter }) => ({
+      counter: counter + 1,
+    }));
+  }
+
+  render() {
+    const { counter } = this.state;
+    if (counter === 5) {
+      throw new Error('I crashed');
+    }
+    return <Button onClick={this.handleClick}>{counter}</Button>;
+  }
+}
+
+export default BuggyCounter;
