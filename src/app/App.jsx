b7adc088dff05e3e692a867ab7bb2ffd5737bea7
Vikash Sharma <vikash@mpaani.com>
2019-11-15 16:59:13 +0530

    Made a single store and pass Provider in app wrapped the app with Provider

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,22 +4,48 @@ import {
 } from 'react-bootstrap';
 // import Calculator from './components/Calculator';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import AddToDo from './components/ToDoList';
 import SimpleCounter from './components/SimpleCounter';
 import './App.scss';
 
-const initState = { count: 0 };
-const initStateForToDo = { add: '' };
+const initState = {
+  count: 0,
+  add: '',
+};
+// const initStateForToDo = { add: '' };
 
 
-function reducerForToDoList(state = initStateForToDo, action) {
-  // console.log(state.add);
-  // state.add.map((v) => console.log(v));
-  // console.log(action);
+// function reducerForToDoList(state = initStateForToDo, action) {
+//   // console.log(state.add);
+//   // state.add.map((v) => console.log(v));
+//   // console.log(action);
+//   switch (action.type) {
+//     case 'ADD_TODO':
+//       return {
+//         add: `${state.add.concat(action.add)}`,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+function aSimplereducer(state = initState, action) {
+  // console.log(state);
   switch (action.type) {
+    case 'INC':
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case 'DEC':
+      return {
+        ...state,
+        count: state.count - 1,
+      };
     case 'ADD_TODO':
       return {
+        ...state,
         add: `${state.add.concat(action.add)}`,
       };
     default:
@@ -27,26 +53,12 @@ function reducerForToDoList(state = initStateForToDo, action) {
   }
 }
 
-function reducerForSimpleCounter(state = initState, action) {
-  // console.log(action);
-  switch (action.type) {
-    case 'INC':
-      return {
-        count: state.count + 1,
-      };
-    case 'DEC':
-      return {
-        count: state.count - 1,
-      };
-    default:
-      return state;
-  }
-}
+// const storeForSimpleCounter = createStore(reducerForSimpleCounter);
+// const storeForToDoList = createStore(reducerForToDoList);
 
-const storeForSimpleCounter = createStore(reducerForSimpleCounter);
-const storeForToDoList = createStore(reducerForToDoList);
+export const store = createStore(aSimplereducer);
 
-// store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => store.getState());
 
 class App extends React.PureComponent {
   render() {
@@ -54,10 +66,10 @@ class App extends React.PureComponent {
       <Container fluid className="App">
         <h2 className="calculator">Simple Counter & ToDo List</h2>
         <Row>
-          <Col><Provider store={storeForSimpleCounter}><SimpleCounter /></Provider></Col>
+          <Col><SimpleCounter /></Col>
         </Row>
         <Row>
-          <Col><Provider store={storeForToDoList}><AddToDo /></Provider></Col>
+          <Col><AddToDo /></Col>
         </Row>
       </Container>
     );
